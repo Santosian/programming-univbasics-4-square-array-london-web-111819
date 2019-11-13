004e7e78d9804e6fedad7d48a6087c2720c63486
Santiago Molinari <santomolinari@gmail.com>
2019-11-13 16:09:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+
+
+def square_array(array) 
+  numbers = ["1"]
+  counter = 0
+  while numbers[counter]< 3 do
+    print Math.sqrt(numbers)
+    counter += 1
+  end
 end
+
